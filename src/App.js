866296163769013ff740ea95d4c1a819fd34a407
Mouhammadou Oury Diallo <ryhow99@gmail.com>
2021-08-15 10:35:25 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+//import React, { Component } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   return(
+     <div className="App">
+       <h1>Hello World!</h1>
+      <div class="container border">
+        <form class="mt-5 mb-5">
+          <div class="form-group">
+            <label for="lastname">Last Name:</label>
+            <input type="text" placeholder="enter your Last name" class="form-control" name="lastname" id=""/><br></br>
+          </div>
+            
+          <div class="form-group">
+            <label for="name">First Name:</label>
+            <input type="text" placeholder="enter your First name" class="form-control" name="name" id=""/><br></br>
+          </div>
+
+          <div class="form-group">
+            <label for="mail">Email:</label>
+            <input type="text" placeholder="enter your name" class="form-control" name="mail" id=""/> <br></br>
+          </div>
+
+            <button className="btn btn-primary">Soumettre</button>
+
+        </form>
+      </div>
+
     </div>
-  );
+
+
+);
+
 }
 
 export default App;
---
+++ src/test.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export default class test extends Component {
+    render() {
+        return (
+            <div>
+                div class="container">
+
+<form>
+  <label for="lastname">Last Name:</label>
+  <input type="text" placeholder="enter your Last name" name="lastname" id=""></input>
+
+  <label for="name">First Name:</label>
+  <input type="text" placeholder="enter your First name" name="name" id=""></input>
+
+  <label for="mail">Email:</label>
+  <input type="text" placeholder="enter your name" name="mail" id=""></input>
+
+  <input type="submit">Soumettre</input>
+
+</form>
+
+</div>
+            </div>
+        )
+    }
+}
